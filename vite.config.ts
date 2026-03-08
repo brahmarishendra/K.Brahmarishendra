3120d1094dec42040aa085e0ba755398194071bc
Your Name <you@example.com>
2026-03-08 17:22:07 +0530

    make base path conditional for Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/K.Brahmarishendra/',
+  base: process.env.GITHUB_ACTIONS ? '/K.Brahmarishendra/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
